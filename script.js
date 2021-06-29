811147eddb9b6e09279d7e13e4af47df39062e4b
Clepter <84567842+ClepterLT@users.noreply.github.com>
2021-06-29 17:26:50 +0300

    Login implementation

--- script.js
+++ script.js
@@ -60,3 +60,206 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+
+  movements.forEach(function (mov, i) {
+    const movementType = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${movementType}">${
+      i + 1
+    } ${movementType}</div>
+        <div class="movements__value">${mov}</div>
+    </div>
+    `;
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+
+const calcDisplayBalance = function (mov) {
+  const balance = mov.reduce((acc, curr) => acc + curr, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const deposits = acc.movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${deposits}€`;
+
+  const withdrawals = acc.movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(withdrawals)}€`;
+
+  const interest = acc.movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit / 100) * acc.interestRate)
+    .reduce((acc, int) => acc + int);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((nameWord) => nameWord[0])
+      .join("");
+  });
+};
+createUsernames(accounts);
+
+/////////////////////////////////////////////////
+// EVENT HANDLERS
+
+let currentAccount;
+
+btnLogin.addEventListener("click", function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    (acc) => acc.username === inputLoginUsername.value
+  );
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and welcome message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(" ")[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear the input fields
+    inputLoginUsername.value = inputLoginPin.value = "";
+    inputLoginPin.blur();
+
+    // Calculate and display movements
+    displayMovements(currentAccount.movements);
+
+    // Display balance
+    calcDisplayBalance(currentAccount.movements);
+
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  }
+});
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+// MAP method
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUSD = 1.1;
+
+const movementsUSD = movements.map((mov) => mov * eurToUSD);
+console.log(movements);
+console.log(movementsUSD);
+*/
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter((mov) => mov > 0);
+console.log(deposits);
+
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
+*/
+
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const balance = movements.reduce(function (acc, curr, i, arr) {
+  return acc + curr;
+}, 0);
+
+console.log(balance);
+*/
+
+// Maximum array value
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const maxMovement = movements.reduce(
+  (acc, curr) => (acc > curr ? acc : curr),
+  movements[0]
+);
+console.log(maxMovement);
+*/
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+/*
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map((dogAge) =>
+    dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4
+  );
+
+  const humanAgesFiltered = humanAges.filter((humanAge) => humanAge >= 18);
+
+  const averageAge =
+    humanAgesFiltered.reduce((acc, age) => acc + age, 0) /
+    humanAgesFiltered.length;
+
+  return averageAge;
+};
+
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+/*
+const calcAverageHumanAge = (dogAgesArr) =>
+  dogAgesArr
+    .map((dogAge) => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
+    .filter((humanAge) => humanAge >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+    */
+
+/*
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map((dogAge) =>
+    dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4
+  );
+
+  const humanAgesFiltered = humanAges.filter((humanAge) => humanAge >= 18);
+
+  const averageAge =
+    humanAgesFiltered.reduce((acc, age) => acc + age, 0) /
+    humanAgesFiltered.length;
+
+  return averageAge;
+};
+*/
+
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
